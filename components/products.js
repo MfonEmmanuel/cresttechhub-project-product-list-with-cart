c74bba9ee2d5855a9779938e7e5dc4e2ae76072f
Emmanuel <mfonemmanuel@icloud.com>
2025-03-07 03:52:27 +0100

    initial commit

--- components/products.js
+++ components/products.js
@@ -11,7 +11,7 @@ import {
 } from "./ui/card";
 import { Button } from "./ui/button";
 import { ShoppingCart } from "lucide-react";
-import { useContext, useState, useCallback } from "react";
+import { useContext, useState } from "react";
 import { CartContext } from "@/context/cart";
 import Cart from "./cart";
 
@@ -27,33 +27,36 @@ export default function Products() {
     <section>
       <div className="mb-8 flex flex-wrap items-center justify-between gap-4">
         <h1 className="text-3xl font-bold">Products</h1>
-        <Button onClick={toggle}>Cart ({cartItems.length})</Button>
+        <Button onClick={toggle}>Cart ({cartItems && cartItems.length})</Button>
       </div>
 
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
-        {products.map((product, index) => (
-          <Card key={index}>
-            <CardHeader>
-              <Image
-                src={product.image.desktop}
-                alt={product.name}
-                width={1920}
-                height={1080}
-                className="w-full rounded-lg"
-              />
-              <Button onClick={() => addToCart(product)}>
-                <ShoppingCart />
-                Add to cart
-              </Button>
-            </CardHeader>
+        {products ? (
+          products.map((product, index) => (
+            <Card key={index}>
+              <CardHeader>
+                <Image
+                  src={product.image.desktop}
+                  alt={product.name}
+                  width={1920}
+                  height={1080}
+                  className="w-full rounded-lg"
+                />
+                <Button onClick={() => addToCart(product)}>
+                  <ShoppingCart /> Add to cart
+                </Button>
+              </CardHeader>
 
-            <CardContent className="space-y-2">
-              <CardDescription>{product.category}</CardDescription>
-              <CardTitle>{product.name}</CardTitle>
-              <CardDescription>${product.price.toFixed(2)}</CardDescription>
-            </CardContent>
-          </Card>
-        ))}
+              <CardContent className="space-y-2">
+                <CardDescription>{product.category}</CardDescription>
+                <CardTitle>{product.name}</CardTitle>
+                <CardDescription>${product.price.toFixed(2)}</CardDescription>
+              </CardContent>
+            </Card>
+          ))
+        ) : (
+          <p className="mt-8 text-center">Loading products...</p>
+        )}
       </div>
 
       {showCart && <Cart toggle={toggle} />}
